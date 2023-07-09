7fb524b6d1e6e071bc817a56434685aee0a98b1f
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-07-09 16:42:22 -0700

    Add default high score script

--- assets/quizscript.js
+++ assets/quizscript.js
@@ -1,2 +1,11 @@
 // Element selectors
 var highScoreElt = document.querySelector("#high-score");
+var displayElt = document.querySelector("#display");
+
+// displayHighScore function
+function displayHighScore() {
+    displayElt.appendChild("<h2>The high score is 99</h2>");
+}
+
+// Add event listener to the high score button
+highScoreElt.addEventListener("click", displayHighScore());
